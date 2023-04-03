919eb82e6519369ebcecc32d5aa0650d74f57935
Olalekan Lasabi <lekanlasabi@gmail.com>
2023-04-02 17:22:19 -0700

    navbar

---
+++ src/Navbar.js
@@ -0,0 +1,63 @@
+import React, { useState, useRef, useEffect } from 'react'
+import { FaBars } from 'react-icons/fa'
+import { links, social } from './data'
+import logo from './logo.svg'
+
+const Navbar = () => {
+  const [showLink, setShowLink] = useState(false)
+  const linksContainerRef = useRef(null)
+  const linksRef = useRef(null)
+
+  useEffect(()=>{
+    const linksHeight = linksRef.current.getBoundingClientRect().height
+    if(showLink){
+      linksContainerRef.current.style.height = `${linksHeight}px`
+    }else{
+      linksContainerRef.current.style.height = '0px'
+    }
+  }, [showLink])
+  return <nav>
+    <div className='nav-center'>
+      <div className='nav-header'>
+        <img src={logo} alt='logo'/>
+        <button className='nav-toggle' onClick={()=>setShowLink(!showLink)}><FaBars /></button>
+      </div>
+      {/* {showLink && (
+        <div className='links-container show-container'>
+        <ul className='links'>
+          {links.map((link)=>{
+            const {id, url, text} = link
+            return (
+            <li key={id}>
+              <a href={url}>{text}</a>
+            </li>)
+          })}
+        </ul>
+      </div>
+      )} */}
+      <div ref={linksContainerRef} className={`${showLink ? 'links-container show-container': 'links-container'}`}>
+        <ul className='links' ref={linksRef}>
+          {links.map((link)=>{
+            const {id, url, text} = link
+            return (
+            <li key={id}>
+              <a href={url}>{text}</a>
+            </li>)
+          })}
+        </ul>
+      </div>
+      <ul className='social-icons'>
+        {social.map((link)=>{
+          const {id, url, icon} = link
+          return(
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  </nav>
+}
+
+export default Navbar
